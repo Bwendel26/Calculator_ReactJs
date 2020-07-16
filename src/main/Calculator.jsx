2d5955fd92a7d7e92db20928e2d39f6cce983a7e
Bruno Fernandes <bwendel26999@gmail.com>
2020-07-16 00:21:45 -0300

    adding the logic of showing values in the calculators display and storage our inputed values... the . and left zeros logic was implemented in the addDigit function

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,17 @@ import "./Calculator.css"
 import Button from "./../components/Button"
 import Display from "./../components/Display"
 
+const initialState = {
+   displayValue: "0", //initial
+   clearDisplay: false,
+   operation: null,
+   values: [0, 0],
+   current: 0 //change de index of the array above.
+}
+
 export default class Calculator extends Component {
+
+   state = { ...initialState } //clone the initialState object and atributed to state
 
    constructor(props) {
       super(props)
@@ -14,19 +24,38 @@ export default class Calculator extends Component {
    }
 
    clearMemory(){
-      console.log("Clean")
+      this.setState({ ...initialState })
    }
    setOperation(operation){
       console.log(operation)
    }
    addDigit(n){
-      console.log(n)
+      if (n === "." && this.state.displayValue.includes(".")){
+         return //ignore if you try to add more than one "."
+      }
+      //rule to takeoff the left zeros:
+      const clearDisplay = this.state.displayValue === "0"
+         || this.state.clearDisplay 
+      
+      const currentValue = clearDisplay ? "" : this.state.displayValue
+      const displayValue = currentValue + n
+      this.setState({ displayValue, clearDisplay: false })
+         //same that displayValue: displayValue
+
+      if (n !== "."){
+         const i = this.state.current //index
+         const newValue = parseFloat(displayValue)
+         const values = [...this.state.values]
+         values[i] = newValue
+         this.setState({ values })
+            //same that values: values
+      }
    }
 
    render() {
       return (
          <div className="calculator">
-            <Display value={100}/>
+            <Display value={this.state.displayValue}/>
             <Button label="AC" click={this.clearMemory} triple/>
             <Button label="/" click={this.setOperation} operation/>
             <Button label="7" click={this.addDigit}/>
